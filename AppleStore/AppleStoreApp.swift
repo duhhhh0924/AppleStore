b1819201f0e4ea23f7072e8868baa5995fc0c603
刘海洋 <1292583000@qq.com>
2023-04-06 13:32:27 +0800

    Initial Commit

---
+++ AppleStore/AppleStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleStoreApp.swift
+//  AppleStore
+//
+//  Created by 移动平台 on 2023/4/6.
+//
+
+import SwiftUI
+
+@main
+struct AppleStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleStore/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AppleStore
+//
+//  Created by 移动平台 on 2023/4/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
